c8823ad8962c428a387704620bac6e00adf698f2
Greg Besson <bessong@gmail.com>
2024-09-19 19:20:38 +0200

    host

--- src/auth/create-top-level-redirect.ts
+++ src/auth/create-top-level-redirect.ts
@@ -6,8 +6,8 @@ import redirectionPage from './redirection-page';
 
 export default function createTopLevelRedirect(apiKey: string, path: string) {
   return function topLevelRedirect(ctx: Context) {
-    const {host, query} = ctx;
-    const {shop} = query;
+    const {query} = ctx;
+    const {shop, host} = query;
 
     const params = {shop};
     const queryString = querystring.stringify(params);
